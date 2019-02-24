6f8e868ecf1d75c0225ecfc6ff1e9b39a880233c
Kristian Heruc <kristian@heruc.com>
2019-02-24 15:08:13 +0100

    Add custom _app.js with sign in methods and UserContext

---
+++ pages/_app.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import App, { Container } from 'next/app';
+import Router from 'next/router';
+import UserContext from '../components/UserContext';
+
+export default class MyApp extends App {
+  state = {
+    user: null
+  };
+
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  componentDidMount = () => {
+    const user = localStorage.getItem('coolapp-user');
+    if (user) {
+      this.setState({
+        user
+      });
+    } else {
+      Router.push('/signin');
+    }
+  };
+
+  signIn = (username, password) => {
+    localStorage.setItem('coolapp-user', username);
+
+    this.setState(
+      {
+        user: username
+      },
+      () => {
+        Router.push('/');
+      }
+    );
+  };
+
+  signOut = () => {
+    localStorage.removeItem('coolapp-user');
+    this.setState({
+      user: null
+    });
+    Router.push('/signin');
+  };
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <UserContext.Provider value={{ user: this.state.user, signIn: this.signIn, signOut: this.signOut }}>
+          <Component {...pageProps} />
+        </UserContext.Provider>
+      </Container>
+    );
+  }
+}
