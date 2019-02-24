5d807c0d43f3ad17b69cc93899f9732373dee553
Kristian Heruc <kristian@heruc.com>
2019-02-24 15:10:13 +0100

    Add UserInfo component and include it on the homepage

---
+++ components/UserInfo.js
@@ -0,0 +1,30 @@
+import { useContext } from 'react';
+import UserContext from '../components/UserContext';
+
+const UserInfo = () => {
+  const { user, signOut } = useContext(UserContext);
+
+  return (
+    <div className="user-info">
+      <p>
+        Hello, <strong>{user}</strong>
+      </p>
+      <p>Welcome to our app</p>
+      <button className="btn" onClick={signOut}>
+        Sign Out
+      </button>
+
+      <style jsx>{`
+        .user-info {
+          padding: 20px;
+          text-align: center;
+        }
+        .btn {
+          margin-top: 20px;
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export default UserInfo;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Layout from '../components/Layout';
+import UserInfo from '../components/UserInfo';
 
 const Index = () => {
   return (
     <Layout>
       <h1>Cool App</h1>
+      <UserInfo />
     </Layout>
   );
 };
